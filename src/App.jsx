f944a96cebbd1b1be9bc251a0458e4b271b51e4e
Aditya Shee <161288779+AdityaShee12@users.noreply.github.com>
2026-01-14 18:31:27 +0530

    Correct import code in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import Skills from "./pages/Skills";
-import Projects from "./pages/Projects";
-import Contact from "./pages/Contact";
+import Navbar from "./components/Navbar.jsx";
+import Home from "./pages/Home.jsx";
+import Skills from "./pages/skills.jsx";
+import Projects from "./pages/Projects.jsx";
+import Contact from "./pages/Contact.jsx";
 
 function App() {
   return (
